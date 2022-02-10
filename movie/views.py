d711da0a112e00240f61dd48fd7c5d95e4b308c1
mjgutierre <mjgutierre@eafit.edu.co>
2022-02-10 17:49:05 -0500

    Subi algo nuevo

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html', {'name':'Greg Lim'})
+    return render(request,'home.html', {'name':'Maria Jose Gutiérrez'})
 
 
 # Create your views here.
