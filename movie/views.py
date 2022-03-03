6244ddea40735b88d4d2398ce37c90decae61d84
mjgutierre <mjgutierre@eafit.edu.co>
2022-03-03 10:58:54 -0500

    Se agrego formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html', {'name':'Maria Jose Gutiérrez'})
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    searchTerm = request.GET.get('searchMovie')
+    return render(request ,'home.html', {'searchTerm':searchTerm})
 
 
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
 # Create your views here.
